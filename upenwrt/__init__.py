94b08f40c78aa1b9fdaaf79e4bc44467c3b552cc
Ivan Shapovalov <intelfx@intelfx.name>
2019-10-13 18:14:38 +0300

    upenwrt: UpenwrtHTTP*: port to aiohttp as is
    Port to aiohttp as is, with fully synchronous request processing.

--- upenwrt/__init__.py
+++ upenwrt/__init__.py
@@ -4,10 +4,10 @@ import os
 import os.path as p
 import argparse
 
-from .server import UpenwrtHTTPServer, UpenwrtContext, UpenwrtHTTPRequestHandler
+from .server import UpenwrtContext, UpenwrtApp, upenwrt_serve
 
 
-def main(argv=None):
+def parse_args(argv=None):
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-l', '--listen', default='0.0.0.0')
 	parser.add_argument('-p', '--port', type=int, default=8000)
@@ -15,11 +15,20 @@ def main(argv=None):
 	parser.add_argument('-b', '--baseurl', default='http://localhost:8000')
 	args = parser.parse_args(args=argv)
 
-	httpd_address = (args.listen, args.port)
-	httpd_context = UpenwrtContext.from_args(
+	context = UpenwrtContext.from_args(
 		basedir=p.join(os.getcwd(), args.basedir),
 		baseurl=args.baseurl,
 	)
 
-	httpd = UpenwrtHTTPServer(httpd_address, UpenwrtHTTPRequestHandler, context=httpd_context)
-	httpd.serve_forever()
+	return args, context
+
+
+def upenwrt():
+	args, context = parse_args()
+	return UpenwrtApp(context=context)
+
+
+def main(argv=None):
+	args, context = parse_args(argv=argv)
+	app = UpenwrtApp(context)
+	upenwrt_serve(host=args.listen, port=args.port, app=app)
