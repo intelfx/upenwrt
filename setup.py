8f21c09e295550722a8f5280537b909d6b4a0e01
Ivan Shapovalov <intelfx@intelfx.name>
2019-10-15 04:15:55 +0300

    setup.py: minimal provisions for packaging & distribution
    As of now, static files in root/ are included in the source distribution
    but are not installed automatically. It is up to the user to install
    them into a suitable directory and invoke the daemon accordingly.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name='upenwrt',
+	version='0.1.0',
+	author='Ivan Shapovalov',
+	author_email='intelfx@intelfx.name',
+	description='A tool that generates custom OpenWRT sysupgrade images',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/intelfx/upenwrt',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: GNU Affero General Public License v3",
+		"Development Status :: 3 - Alpha",
+	],
+	python_requires='>=3.7',
+	install_requires=[
+		'requests',
+		'urllib3',
+		'asyncio',
+		'aiohttp',
+		'aiofiles',
+	],
+	include_package_data=True,
+)
